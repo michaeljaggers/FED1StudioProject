f629df7e6f9be8d0a19c5c8ecb4a367b8db7a1c7
Michael Jaggers <michael.jaggers@gmail.com.com>
2021-03-02 12:21:45 -0600

    Add jQeury and a lightbox to the team.html page

---
+++ FED1StudioProject/scripts/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#myTable').DataTable();
+
+    $(".thumb").on("click", function () {
+        
+        var imgSrc = $(this).attr("src");
+
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive"/>');
+
+        $("#lightbox-container").fadeIn(500);
+
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+    });
+});
